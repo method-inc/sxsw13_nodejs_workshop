69180204b474defb4b3e4a5d51b37f14f80ba817
Jim Snodgrass <jim@skookum.com>
2013-02-28 09:21:59 -0500

    more javascript and v8 specific concepts

---
+++ javascript_concepts/class.js
@@ -0,0 +1,103 @@
+/**
+ * Person class
+ * @author Jim Snodgrass
+ * @property {String} name the person's name
+ * @property {Number} walk_speed the person's walking speed
+ * @property {Number} run_speed the person's running speed
+ */
+
+function Person(options) {
+
+  // options needs to be initialized with a default if none were passed in
+
+  options = options || {};
+
+  // instance properties
+  // the use of guards makes sure the property is initialized as the correct type if
+  // that particular option was not passed in
+
+  this.name = options.name || "Jon Doe";
+  this.walk_speed = options.walk_speed || 5;
+  this.run_speed = options.run_speed || 12;
+
+
+}
+
+// Now we defined methods on the class by definining it's prototype
+// you could also start with another class's prototype as a base
+
+Person.prototype = {
+
+  speak: function speak() {
+    console.log(this.name + " speaks");
+  },
+
+  walk: function walk() {
+    console.log(this.name + " is walking at a speed of " + this.walk_speed);
+  },
+
+  run: function run() {
+    console.log(this.name + " is running at a speed of " + this.run_speed);
+  }
+
+};
+
+
+
+var John = new Person({
+  name: "John Doe",
+  walk_speed: 6,
+  run_speed: 11
+});
+
+
+John.walk();
+
+// John Doe is walking at a speed of 6
+
+John.run();
+
+// John Doe is running at a speed of 11
+
+
+
+
+
+
+
+/**
+ * Male class
+ */
+function Male(options) {
+  options = options || {};
+
+  this.gender = 'male';
+}
+
+// important to know that the Person class is initialized at this time which might be undesirable
+Male.prototype = new Person();
+
+// override method
+Male.prototype.speak = function(says) {
+  says = says || "nothing";
+  console.log(this.name + " says " + says);
+};
+
+
+/**
+ * Female class
+ */
+function Female(options) {
+  options = options || {};
+
+  this.gender = 'female';
+}
+
+// better way that doesn't initialize Person class immediately
+Female.prototype._proto_ = Person.prototype;
+
+// override method
+Female.prototype.speak = function(says) {
+  says = says || "nothing";
+  console.log(this.name + " says " + says);
+};
---
+++ javascript_concepts/v8_methods.js
@@ -0,0 +1,107 @@
+/**
+ * Getting the keys of an object
+ * without iterating through and using hasOwnProperty
+ */
+var book = {
+  name: "The Hitchhiker's Guide to the Galaxy",
+  author: "Douglas Adams",
+  num_pages: 224
+};
+
+Object.keys(book);
+//[ 'name', 'author', 'num_pages' ]
+
+
+
+/**
+ * accurately checking for an array type
+ */
+var array = [];
+
+var type = typeof array1;
+// object
+
+Array.isArray(array1);
+// true
+
+
+
+/**
+ * native array iteration
+ */
+
+["jim", "david", 23].forEach( function(value) {
+  console.log(value);
+});
+// jim
+// david
+// 23
+
+
+/**
+ * native array filter
+ */
+
+["jim", "david", 23].filter( function(value) {
+  return typeof value == "string";
+});
+// [ 'jim', 'david' ]
+
+
+/**
+ * Native string trim
+ */
+
+"  hello world ".trim();
+// 'hello world'
+
+
+
+/**
+ * function binding
+ */
+function a() {
+  return this.hello == 'world';
+}
+
+a();
+// false
+
+var b = { hello: 'world' };
+
+var c = a.bind(b); // sets scope of 'this' to b
+
+c();
+// true
+
+
+
+// Accessor methods (__defineGetter__, __defineSetter__)
+
+Date.prototype.__defineGetter__('ago', function() {
+  // define logic to return string for how long ago the date was
+    var diff = new Date() - this;
+
+    var conversions = [
+      ["years", 31518720000],
+      ["months", 2626560000 /* assumes there are 30.4 days in a month */],
+      ["days", 86400000],
+      ["hours", 3600000],
+      ["minutes", 60000],
+      ["seconds", 1000]
+    ];
+
+    for (var i = 0; i < conversions.length; i++) {
+      var result = Math.floor(diff / conversions[i][1]);
+      if (result >= 2) {
+        return result + " " + conversions[i][0] + " ago";
+      }
+    }
+
+    return "just now";
+});
+
+var my_bd = new Date('3/24/1978');
+
+my_bd.ago;
+// '34 years ago'
